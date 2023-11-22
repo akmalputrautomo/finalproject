b265d10f963e35e7ec51714ed935030101cfa243
Heri Kurniawan <heri.kurnwan12@gmail.com>
2023-11-22 15:30:29 +0700

    Slicing Ui Login

--- src/routes/RouterList.jsx
+++ src/routes/RouterList.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Register } from "../pages/auth/Register";
+import { Login } from "../pages/auth/Login";
+import { Provider } from "react-redux";
 
 export const RouterList = () => {
   return (
     <BrowserRouter>
+    {/* <Provider store={store}> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
+    {/* </Provider> */}
     </BrowserRouter>
   );
 };

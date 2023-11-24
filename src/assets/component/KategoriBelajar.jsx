eab5d24dda4df401ef96ceb6609dbabf20f589f2
afifahnn <141509819+afifahnn@users.noreply.github.com>
2023-11-24 18:01:20 +0700

    KategoriBelajar

---
+++ src/assets/component/KategoriBelajar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const KategoriBelajar = () => {
+  return (
+    <div className="bg-[#CFE2E080] px-[9rem] py-[1.5rem]">
+      <div className="flex justify-between">
+        <span className="text-[1.8rem] font-bold text-[#000000]">Kategori Belajar</span>
+        <button className="text-[1.2rem] font-bold text-[#116E63] hover:opacity-80">Lihat Semua</button>
+      </div>
+
+      {/* image map */}
+      {/* <div>
+        <div className="py-[1rem]">gambar</div>
+        <span className="text-[#000000] text-[1.1rem]">judul kategori</span>
+      </div> */}
+    </div>
+  );
+};

3cf0fc3f3ecc5eb18f32b240c3e97f03844587ff
akmalputrautomo <apu.utomo@gmail.com>
2023-12-01 01:26:17 +0700

    web notifikasi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import Navbar from "../assets/components/navbar";
 import { KategoriBelajar } from "../assets/components/KategoriBelajar";
 import Hero from "../assets/components/Hero";
 import NavbarAfterLogin from "../assets/components/NavbarAfterLogin";
+import WebNotifikasi from "../assets/components/WebNotifikasi";
 
 export const Home = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <KategoriBelajar />
-      <NavbarAfterLogin />
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      {/* <KategoriBelajar /> */}
+      {/* <NavbarAfterLogin /> */}
+      <WebNotifikasi />
     </div>
   );
 };

e6d7f2b16cdbf73cd6536ed8dae24fc06c96553e
Heri Kurniawan <heri.kurnwan12@gmail.com>
2023-12-05 15:23:57 +0700

    Nav & LoginAdm

---
+++ src/pages/HomeAdm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarAdm from '../assets/components/NavbarAdm'
+import CardAdm from '../assets/components/CardAdm'
+
+export const HomeAdm = () => {
+  return (
+    <>
+    <NavbarAdm/>
+    <CardAdm/>
+    </>
+  )
+}
--- src/routes/RouterList.jsx
+++ src/routes/RouterList.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
-import { Register } from "../pages/auth/Register";
-import { Login } from "../pages/auth/Login";
-import { LoginUser } from "../pages/auth/LoginUser";
+import { Register } from "../auth/user/Register";
+import { Login } from "../auth/user/Login";
+import { LoginUser } from "../auth/user/LoginUser";
 import { Provider } from "react-redux";
-import OtpRegister from "../pages/auth/Otpregister";
-import { ForgetPass } from "../pages/auth/ForgetPass";
+import OtpRegister from "../auth/user/Otpregister";
+import { ForgetPass } from "../auth/user/ForgetPass";
 import { BerandaKelasSaya } from "../pages/BerandaKelasSaya";
 import { BerandaKelas } from "../pages/BerandaKelas";
 import { DetailKelas } from "../assets/components/DetailKelas";
 import { DetailKelasPembayaran } from "../assets/components/DetailKelasPembayaran";
+import { LoginAdmin } from "../auth/admin/LoginAdmin";
+import { HomeAdm } from "../pages/HomeAdm";
 
 export const RouterList = () => {
   return (
@@ -27,6 +29,11 @@ export const RouterList = () => {
         <Route path="/berandakelas" element={<BerandaKelas/>}/>
         <Route path="/detailKelas" element={<DetailKelas />} />
         <Route path="/detailKelasPembayaran" element={<DetailKelasPembayaran />} />
+
+        {/* Admin */}
+        <Route path="/loginAdm" element={<LoginAdmin />} />
+        <Route path="/HomeAdm" element={<HomeAdm />} />
+        {/* Admin End */}
       </Routes>
       {/* </Provider> */}
     </BrowserRouter>

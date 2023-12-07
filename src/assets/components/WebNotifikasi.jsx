e7e2df3a905c32336e1eea88698e1668b8d2d2a6
akmalputrautomo <apu.utomo@gmail.com>
2023-12-08 00:03:45 +0700

    responsive notifikasi

--- src/assets/components/WebNotifikasi.jsx
+++ src/assets/components/WebNotifikasi.jsx
@@ -7,70 +7,72 @@ const WebNotifikasi = () => {
 
   return (
     <div>
-      <NavbarNotifikasi />
-      <div className="w-full h-[10rem] bg-[#E7F0EF] text-[16px] font-bold">
+      <div className="mobile:hidden desktop:block">
+        <NavbarNotifikasi />
+      </div>
+      <div className="w-full h-[10rem] bg-[#E7F0EF] text-[16px] font-bold mobile:hidden desktop:block">
         <button
           onClick={() => {
             navigate();
           }}
-          className="text-[#116E63] flex gap-2 items-center text-[20px] font-serif pl-[10rem] pt-6"
+          className="text-[#116E63] flex gap-2 items-center text-[20px] font-serif pl-[10rem] pt-6 mobile:hidden desktop:block"
         >
           <i class="fa-solid fa-arrow-left"> </i>
           kembali ke beranda
         </button>
       </div>
 
-      <div className="flex justify-center items-center mt-[-4rem]  ">
-        <div className="w-[75%] h-[30rem] flex border border-[#116E63] rounded-md flex-col ">
-          <div className="w-full bg-[#116E63] h-[4rem] flex justify-center items-center ">
-            <h1 className="text-white font-bold text-2xl">Notifikasi</h1>
+      <div className="flex justify-center items-center mt-[-4rem] mobile:mt-0 desktop:mt-[-4rem] ">
+        <div className="w-[75%] h-[30rem] flex border border-[#116E63] rounded-md flex-col mobile:w-full desktop:w-[75%] mobile:h-screen desktop:h-[30rem]">
+          <div className="w-full bg-[#116E63] h-[4rem] flex justify-center items-center mobile:justify-start pl-4 desktop:justify-center">
+            <h1 className="text-white font-bold text-2xl ">Notifikasi</h1>
           </div>
-          <div className="flex flex-col gap-10 pt-4 pl-10">
+          <div className="flex flex-col gap-10 pt-4 pl-10 mobile:pl-4 desktop:pl-10">
             <div className="flex gap-2">
               <div className="bg-[#116E63] text-white w-[1.5rem] h-[1.5rem] flex justify-center items-center rounded-md ">
                 <i class="fa-regular fa-bell"></i>
               </div>
-              <div className="flex justify-between w-full pr-10">
-                <div className="flex flex-col">
+              <div className="flex justify-between w-full pr-10 mobile:pr-0 desktop:pr-10">
+                <div className="flex flex-col w-full ">
                   <h1 className="text-[#116E63]">Promosi</h1>
-                  <h1>Dapatkan Potongan 50% selama Bulan Maret! </h1>
-                  <h1>Syarat dan Ketentuan berlaku! </h1>
+                  <p>Dapatkan Potongan 50% selama Bulan Maret! </p>
+                  <h1 className="mobile:text-[#8A8A8A]">Syarat dan Ketentuan berlaku! </h1>
                 </div>
-                <h1>2 Maret, 12:00</h1>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <div className="bg-[#116E63] text-white w-[1.5rem] h-[1.5rem] flex justify-center items-center rounded-md ">
-                <i class="fa-regular fa-bell"></i>
-              </div>
-              <div className="flex justify-between w-full pr-10">
-                <div className="flex flex-col">
-                  <h1 className="text-[#116E63]">Promosi</h1>
-                  <h1>Dapatkan Potongan 50% selama Bulan Maret! </h1>
-                  <h1>Syarat dan Ketentuan berlaku! </h1>
+                <div className="mobile:w-[50%] desktop:w-[15%] desktop:justify-end ">
+                  <h1>2 Maret, 12:00</h1>
                 </div>
-                <h1>2 Maret, 12:00</h1>
               </div>
             </div>
             <div className="flex gap-2">
               <div className="bg-[#116E63] text-white w-[1.5rem] h-[1.5rem] flex justify-center items-center rounded-md ">
                 <i class="fa-regular fa-bell"></i>
               </div>
-              <div className="flex justify-between w-full pr-10">
-                <div className="flex flex-col">
+              <div className="flex justify-between w-full pr-10 mobile:pr-0 desktop:pr-10">
+                <div className="flex flex-col w-full ">
                   <h1 className="text-[#116E63]">Promosi</h1>
-                  <h1>Dapatkan Potongan 50% selama Bulan Maret! </h1>
-                  <h1>Syarat dan Ketentuan berlaku! </h1>
+                  <p>Dapatkan Potongan 50% selama Bulan Maret! </p>
+                  <h1 className="mobile:text-[#8A8A8A]">Syarat dan Ketentuan berlaku! </h1>
                 </div>
-                <h1>2 Maret, 12:00</h1>
+                <div className="mobile:w-[50%] desktop:w-[15%] desktop:justify-end ">
+                  <h1>2 Maret, 12:00</h1>
+                </div>
               </div>
             </div>
-            {/* <div className="bg-[#116E63] text-white w-[1.5rem] h-[1.5rem] flex justify-center items-center rounded-md ">
-              <i class="fa-regular fa-bell"></i>
+            <div className="flex gap-2">
+              <div className="bg-[#116E63] text-white w-[1.5rem] h-[1.5rem] flex justify-center items-center rounded-md ">
+                <i class="fa-regular fa-bell"></i>
+              </div>
+              <div className="flex justify-between w-full pr-10 mobile:pr-0 desktop:pr-10">
+                <div className="flex flex-col w-full ">
+                  <h1 className="text-[#116E63]">Promosi</h1>
+                  <p>Dapatkan Potongan 50% selama Bulan Maret! </p>
+                  <h1 className="mobile:text-[#8A8A8A]">Syarat dan Ketentuan berlaku! </h1>
+                </div>
+                <div className="mobile:w-[50%] desktop:w-[15%] desktop:justify-end ">
+                  <h1>2 Maret, 12:00</h1>
+                </div>
+              </div>
             </div>
-            <div className="bg-[#116E63] text-white w-[1.5rem] h-[1.5rem] flex justify-center items-center rounded-md ">
-              <i class="fa-regular fa-bell"></i>
-            </div> */}
           </div>
         </div>
       </div>
--- src/pages/notifikasi/WebAkunProfil.jsx
+++ src/pages/notifikasi/WebAkunProfil.jsx
@@ -3,37 +3,17 @@ import { NavbarAkun } from "../../assets/components/elements/NavbarAkun";
 import { Navigate } from "react-router-dom";
 
 export const WebAkunProfil = () => {
-  const [states, setStates] = useState({
-    ProfilSaya: true,
-    UbahPassword: false,
-    RiwayatPembayaran: false,
-  });
-
-  function handleclick(clickedState) {
-    setStates((prevState) => {
-      // Mengatur semua state menjadi false
-      const newStates = {
-        ProfilSaya: false,
-        UbahPassword: false,
-        RiwayatPembayaran: false,
-      };
-
-      // Mengatur state yang diklik menjadi true
-      newStates[clickedState] = true;
-
-      return newStates;
-    });
-  }
-
   return (
     <>
-      <NavbarAkun />
+      <div>
+        <NavbarAkun />
+      </div>
       <div className="w-full h-[8rem] bg-[#E7F0EF] text-[16px] font-bold">
         <button
           onClick={() => {
             Navigate();
           }}
-          className="text-[#116E63] flex gap-2 items-center text-[20px] font-serif pl-[10rem] pt-6"
+          className="text-[#116E63] flex gap-2 items-center text-[20px] font-serif pl-[10rem] pt-6  "
         >
           <i class="fa-solid fa-arrow-left"> </i>
           kembali ke beranda
@@ -41,21 +21,21 @@ export const WebAkunProfil = () => {
       </div>
 
       <div className="flex justify-center items-center mt-[-4rem]  ">
-        <div className="w-[65%] h-[37rem] flex border border-[#116E63] rounded-md flex-col ">
+        <div className="w-[65%] h-[37rem] flex border border-[#116E63] rounded-md flex-col  ">
           <div className="w-full bg-[#116E63] h-[4rem] flex justify-center items-center ">
             <h1 className="text-white font-bold text-2xl">Akun</h1>
           </div>
 
           {/* button */}
           <div className="flex w-full ">
-            <div className="w-[50%] flex flex-col items-start gap-10 p-8 ">
-              <button onClick={() => handleclick("ProfilSaya")} className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2 ">
+            <div className="w-[50%] flex flex-col items-start gap-10 p-8  ">
+              <button className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2 ">
                 <i class="fa-solid fa-pen text-[#116E63] text-[1.5rem]  "></i>Profil Saya
               </button>
-              <button onClick={() => handleclick("UbahPassword")} className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2">
+              <button className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2">
                 <i class="fa-solid fa-gear text-[#116E63] text-[1.5rem]"></i>Ubah Password
               </button>
-              <button onClick={() => handleclick("RiwayatPembayaran")} className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2">
+              <button className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2">
                 <i class="fa-solid fa-cart-shopping text-[#116E63] text-[1.5rem]"></i>Riwayat Pembayaran
               </button>
               <button className="text-[1.3rem] w-[80%] flex items-center gap-3 border-b-2">
@@ -66,59 +46,61 @@ export const WebAkunProfil = () => {
             {/* page  */}
             <div className="w-[50%] ">
               {/* Profil Saya */}
-              {states.ProfilSaya && (
-                <div className="flex flex-col gap-4">
-                  <div>
-                    <p>Nama</p>
-                    <input placeholder="John Doe" className="border rounded-xl w-[60%] h-[3rem]"></input>
-                  </div>
-                  <div>
-                    <p>Email</p>
-                    <input placeholder="Johndoe@gmail.com" className="border rounded-xl w-[60%] h-[3rem]"></input>
-                  </div>
-                  <div>
-                    <p>Nomor Telepon</p>
-                    <input placeholder="+62 812121121121" className="border rounded-xl w-[60%] h-[3rem]"></input>
-                  </div>
-                  <div>
-                    <p>Negara</p>
-                    <input placeholder="Masukkan Negara tempat tinggal" className="border rounded-xl w-[60%] h-[3rem]"></input>
-                  </div>
-                  <div>
-                    <p>Kota</p>
-                    <input placeholder="Masukkan kota tempat tinggal" className="border rounded-xl w-[60%] h-[3rem]"></input>
-                  </div>
-                  <div>
-                    <button className="flex justify-center items-center  bg-[#116E63] text-white w-[75%] rounded-xl h-[3rem] text-[1rem] font-bold">Simpan Profil Saya</button>
-                  </div>
+
+              <div className="flex flex-col gap-4 mobile:hidden desktop:flex">
+                <div>
+                  <p>Nama</p>
+                  <input placeholder="John Doe" className="border rounded-xl w-[60%] h-[3rem]"></input>
                 </div>
-              )}
+                <div>
+                  <p>Email</p>
+                  <input placeholder="Johndoe@gmail.com" className="border rounded-xl w-[60%] h-[3rem]"></input>
+                </div>
+                <div>
+                  <p>Nomor Telepon</p>
+                  <input placeholder="+62 812121121121" className="border rounded-xl w-[60%] h-[3rem]"></input>
+                </div>
+                <div>
+                  <p>Negara</p>
+                  <input placeholder="Masukkan Negara tempat tinggal" className="border rounded-xl w-[60%] h-[3rem]"></input>
+                </div>
+                <div>
+                  <p>Kota</p>
+                  <input placeholder="Masukkan kota tempat tinggal" className="border rounded-xl w-[60%] h-[3rem]"></input>
+                </div>
+                <div>
+                  <button className="flex justify-center items-center  bg-[#116E63] text-white w-[75%] rounded-xl h-[3rem] text-[1rem] font-bold">Simpan Profil Saya</button>
+                </div>
+              </div>
+
               {/* ubah password */}
-              {states.UbahPassword && (
-                <div>
-                  <h1 className="font-bold text-2xl flex justify-center pt-4">Ubah Password</h1>
-                  <div className="flex flex-col gap-4 pt-4">
-                    <div>
-                      <p>Masukkan Password Lama</p>
-                      <input type="password" className=" border rounded-xl w-[80%] h-[3rem]"></input>
-                    </div>
-                    <div>
-                      <p>Masukkan Password Baru</p>
-                      <input type="password" className="border rounded-xl w-[80%] h-[3rem]"></input>
-                    </div>
-                    <div>
-                      <p>Ulangi Password Baru</p>
-                      <input type="password" className="border rounded-xl w-[80%] h-[3rem]"></input>
-                    </div>
+
+              {/* <div>
+                <h1 className="font-bold text-2xl flex justify-center pt-4">Ubah Password</h1>
+                <div className="flex flex-col gap-4 pt-4">
+                  <div>
+                    <p>Masukkan Password Lama</p>
+                    <input type="password" className=" border rounded-xl w-[80%] h-[3rem]"></input>
+                  </div>
+                  <div>
+                    <p>Masukkan Password Baru</p>
+                    <input type="password" className="border rounded-xl w-[80%] h-[3rem]"></input>
+                  </div>
+                  <div>
+                    <p>Ulangi Password Baru</p>
+                    <input type="password" className="border rounded-xl w-[80%] h-[3rem]"></input>
+                  </div>
+                  <div className="flex justify-start items-center w-full">
+                    <button className="rounded-full bg-[#116E63] text-white w-[80%] h-[2.5rem]">Ubah Password</button>
                   </div>
                 </div>
-              )}
+              </div> */}
+
               {/* riwayat pembayaran */}
-              {states.RiwayatPembayaran && (
-                <div>
-                  <h1 className="font-bold text-2xl flex justify-center pt-4">Riwayat Pembayaran</h1>
-                </div>
-              )}
+
+              {/* <div>
+                <h1 className="font-bold text-2xl flex justify-center pt-4">Riwayat Pembayaran</h1>
+              </div> */}
             </div>
           </div>
         </div>
